ffdcf54b3ddf4dbba3e7bbfa034f744ff3e32696
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-03-07 10:30:19 +0000

    Created deck and added method to populate deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,33 @@
 public class Deck {
+
+    private Card[] cards;
+
+    public Deck(){
+        this.cards = new Card[52];
+        this.populateDeck();
+    }
+
+    private void populateDeck() {
+        int count = 0;
+        for (Suit suit: Suit.values()){
+            for (Rank rank : Rank.values()){
+                this.cards[count] = new Card(suit, rank);
+                count ++;
+            }
+        }
+    }
+
+    public int countCards() {
+        int count = 0;
+
+        for(int i =0; i< this.cards.length; i++) {
+            if(cards[i] != null) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
+
+
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+
+    @Before
+    public void before() {
+        this.deck = new Deck();
+    }
+
+    @Test
+    public void deckIsPopulatedTest(){
+        assertEquals(52, deck.countCards());
+    }
 }
